783f1a457990f191b2c7a7a8e1da659ccbd2977f
Andrew Klein <andrew.klein.j@gmail.com>
2016-08-03 16:53:00 -0400

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 20, "M", "Brown", "Sweet", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 11, "M", "Cream", "Rowdy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 30, "F", "Polar", "Stern", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 26, "M", "Black", "Angry", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 15, "F", "Black", "Distant", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 40, "M", "Green", "Sour", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 22, "F", "Red", "Delicious", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 13, "F", "Invisible", "Deadly", 1);
